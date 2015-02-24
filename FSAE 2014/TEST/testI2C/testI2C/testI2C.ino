512287c93fbe56623292495ebc8933854b04304f
aman.s.azad@gmail.com <aman.s.azad@gmail.com>
2015-02-24 14:11:05 -0800

    Updated file header

--- FSAE 2014/TEST/testI2C/testI2C/testI2C.ino
+++ FSAE 2014/TEST/testI2C/testI2C/testI2C.ino
@@ -1,21 +1,9 @@
-/**************************************************************************/
-/*!
-    @file     Adafruit_MMA8451.h
-    @author   K. Townsend (Adafruit Industries)
-    @license  BSD (see license.txt)
-
-    This is an example for the Adafruit MMA8451 Accel breakout board
-    ----> https://www.adafruit.com/products/2019
-
-    Adafruit invests time and resources providing this open source code,
-    please support Adafruit and open-source hardware by purchasing
-    products from Adafruit!
-
-    @section  HISTORY
-
-    v1.0  - First release
-*/
-/**************************************************************************/
+/**************************************************************************
+ * File name:     testI2C.ino
+ * Written By:    Aman Azad (2014-2015 FSAE DAQ Team)
+ * Description:   Provide implementation for I2C wiring of gyro
+				  and accelerometer.
+**************************************************************************/
 
 #include <Wire.h>
 #include <Adafruit_MMA8451.h>

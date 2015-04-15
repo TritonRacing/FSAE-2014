918d16d88766cd29d9edd5e655b02b8cf39cb801
amirhajimirsadeghi <amirhajimirsadeghi@gmail.com>
2015-04-14 18:19:01 -0700

    time Extracted

--- GUI/FormulaSAE/src/formulasae/nmeaToLLV.java
+++ GUI/FormulaSAE/src/formulasae/nmeaToLLV.java
@@ -10,6 +10,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * This class will handle reading in a text file that has lines of nmea 
@@ -21,12 +23,35 @@ public class nmeaToLLV {
     private double longitude= 0;
     private double latitude = 0;
     private double velocity = 0;
+    private Date time;
+    
     
     //CONSTRUCTOR
     public nmeaToLLV(String nmeaFilePath) throws FileNotFoundException{
         URL url = getClass().getResource(nmeaFilePath);
         File file = new File(url.getPath());
         InputStream input = new FileInputStream(file);
+    }
+    
+    void extractData(String nmeaSentence){
+        //Array to hold different parts of the sentence
+        String[] sentenceArray = nmeaSentence.split(",");
+
+        //EXTRACT TIME FROM SENTENCE
+        int fullTime = Integer.parseInt(sentenceArray[1]);
+        int second = fullTime%100;
+        fullTime = fullTime/100;
+        int minute = fullTime%100;
+        fullTime = fullTime/100;
+        int hour = fullTime;
+        
+        //initialize time of sentence
+        Calendar timeHolder = Calendar.getInstance();
+        timeHolder.set(Calendar.HOUR_OF_DAY,hour);
+        timeHolder.set(Calendar.MINUTE, minute);
+        timeHolder.set(Calendar.SECOND,second);
+        time = timeHolder.getTime();
+        
     }
     
 }
